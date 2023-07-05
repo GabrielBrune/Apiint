2a9c56197fb31c9e468863f9f0abe183936278a8
brune <gabriels.brunef@gmail.com>
2023-07-04 22:21:43 -0300

    comecando module pedidos

--- src/usuarioadm/usuario.module.ts
+++ src/usuarioadm/usuario.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AdmUserController } from "./usuario.controller";
+import { AdmArmazenado } from "./usuario.dm";
 
 @Module({
     controllers:[AdmUserController],
-    providers:[]
+    providers:[AdmArmazenado]
 })
 
 export class UsuarioModules{}
