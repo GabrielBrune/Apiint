0634c2c189ac5e12d28384fa332d67f156157371
brune <gabriels.brunef@gmail.com>
2023-07-05 19:30:35 -0300

    adição do cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { APP_INTERCEPTOR, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const cors = require('cors');
+  app.use(cors(
+    origin = '*'
+  ));
+  
   await app.listen(3000);
 }
 bootstrap();
