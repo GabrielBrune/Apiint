6446a0077cb1ef7dbc5f8b2fa0f634532271aab5
brune <gabriels.brunef@gmail.com>
2023-08-07 20:28:41 -0300

    auth no import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth/auth.controller';
 import { PontoModules } from './pontos/pontos.module';
 import { UsersModule } from './user/user.module';
 
 
 
 @Module({
-  imports: [PontoModules, UsersModule],
+  imports: [PontoModules, UsersModule, AuthController],
   controllers: [],
   providers: [],
 })
