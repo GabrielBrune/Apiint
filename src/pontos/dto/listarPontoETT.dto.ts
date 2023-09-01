22f49dea1a3d4a85fbf5fe6c69147ce282c17eca
Brune <gabriels,brunef@gmail.com>
2023-08-31 22:23:36 -0300

    alt no listar dto

--- src/pontos/dto/listarPontoETT.dto.ts
+++ src/pontos/dto/listarPontoETT.dto.ts
@@ -1,12 +1,12 @@
 
 export class ListarPontosEttDTO{
     constructor(
-        readonly ID:string,
-        readonly IMG:string,
-        readonly DATA_INICIO: string,
-        readonly DATA_TERMINO: string,
-        readonly DESCRICAO:string,
-        readonly ENTIDADE: string
+        readonly id:string,
+        readonly img:string,
+        readonly dataInicio: string,
+        readonly dataTermino: string,
+        readonly descricao:string,
+        readonly entidade: string
     ){}
 
 }
