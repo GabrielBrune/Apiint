31b9fea25757ac217b878fcd4e83c811a06f2587
EliasKosiski <elias.fkosiski@senacsp.edu.br>
2023-09-05 22:33:42 -0300

    Adicionado usuários

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -12,9 +12,24 @@ export class UsersService{
         },
         {
             userId:2,
-            username: 'userRandom',
-            password: '321',
-        }    
+            username: 'admin',
+            password: '123',
+        },
+        {
+            userId:3,
+            username: 'alefmeireles',
+            password: '123',
+        },
+        {
+            userId:3,
+            username: 'eliaskosiski',
+            password: '123',
+        },
+        {
+            userId:3,
+            username: 'gabrielbrune',
+            password: '123',
+        }      
     ];
     async findOne(username: string): Promise<User | undefined> {
         return this.users.find(user => user.username === username);
